fbd5c56b3dc15636fffaa51e53f12a0d9823ccaf
Corefracture <corefracture@corefracture.com>
2018-12-11 21:05:46 -0500

    (scripting) Updating  with list selector, and playing with some layout stuff on the script editor page. Nasty nasty stuff right now, all kinds of copy paste code in the script editor html

---
+++ web_content/mnemu_script_editor.js
@@ -0,0 +1,16 @@
+/**
+ * @licence
+ * Copyright (C) 2018 Corefracture, Chris Coleman. All Rights Reserved.
+ * www.corefracture.com - @corefracture
+ *
+ * Licensed under the MIT License, https://opensource.org/licenses/MIT
+ * See LICENSE.md for more details
+**/
+
+
+  $( function() {
+    list_sort_test = $( "#sortable" );
+    list_sort_test.sortable();
+    list_sort_test.disableSelection();
+
+  } );
